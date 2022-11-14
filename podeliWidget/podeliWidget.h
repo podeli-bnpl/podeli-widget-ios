dbee792d8d626a5804e39714ade339b14833ccd8
n.malofeev <n.malofeev@podeli.ru>
2022-11-14 16:12:14 +0200

    Initial Commit

---
+++ podeliWidget/podeliWidget.h
@@ -0,0 +1,18 @@
+//
+//  podeliWidget.h
+//  podeliWidget
+//
+//  Created by Николай Малофеев on 14.11.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for podeliWidget.
+FOUNDATION_EXPORT double podeliWidgetVersionNumber;
+
+//! Project version string for podeliWidget.
+FOUNDATION_EXPORT const unsigned char podeliWidgetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <podeliWidget/PublicHeader.h>
+
+
